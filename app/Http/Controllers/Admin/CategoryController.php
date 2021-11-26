f27736031711b31a5a5ead48b9c389f2c67a1d8b
armando-dev <ah25632@gmail.com>
2021-11-26 09:59:24 -0600

    fix category url

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -47,7 +47,7 @@ class CategoryController extends Controller
      */
     public function create()
     {
-        return view("Admin.Category.create");
+        return view("admin.Category.create");
     }
 
     /**
